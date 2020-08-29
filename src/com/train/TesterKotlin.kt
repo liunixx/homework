ac3d021eea209de057e3bf1a2b00fea2e7c0f75d
shajiawang <khliu.tw@gmail.com>
2020-08-29 16:10:55 +0800

    第 1 章，作業 2 - 連續訂票功能 以while  方式  輸入 -1即結束

--- src/com/train/TesterKotlin.kt
+++ src/com/train/TesterKotlin.kt
@@ -4,32 +4,49 @@ import java.util.*
 
 fun main() {
     var numTotal = 0
-    var numRoundTrip=0
+    var numRoundTrip = 0
     // numTotal = Scanner(System.`in`).nextInt()
     try {
-        while (true) {
-            println("\nEnter number of total tickets: ")
-            numTotal = readLine()!!.toInt()
-            if (numTotal == -1) {
-                println("Bye !!")
-                break
-            }
-            println("How many round-trip tickets: ")
-            // numRoundTrip = Scanner(System.`in`).nextInt()
-            numRoundTrip = readLine()!!.toInt()
-            val trainTickets = TrainTickets(numTotal, numRoundTrip)
-            if (trainTickets.checkOk()) {
-                println("Total tickets: ${trainTickets.getTotalTickets()}")
-                println("Round-trip: ${trainTickets.getRoundTripTotal()}")
-                println("Total: ${trainTickets.getTotalPrice()}")
-            }
-        }
+          while(numTotal != -1) {
+              println("Enter number of total tickets: ")
+              numTotal = readLine()!!.toInt()
+              var trainTickets = TrainTickets(numTotal, 0)
+              if(trainTickets.checkOk()) {
+                  println("How many round-trip tickets: ")
+//            numRoundTrip = Scanner(System.`in`).nextInt()
+                  numRoundTrip = readLine()!!.toInt()
+                  trainTickets.setRoundTripTotal(numRoundTrip)
+                  if (trainTickets.checkOk()) {
+                      println("Total tickets: ${trainTickets.getTotalTickets()}")
+                      println("Round-trip: ${trainTickets.getRoundTripTotal()}")
+                      println("Total: ${trainTickets.getTotalPrice()}")
+                  }
+              }
+
+          }
+//        while (true) {
+//            println("\nEnter number of total tickets: ")
+//            numTotal = readLine()!!.toInt()
+//            if (numTotal == -1) {
+//                println("Bye !!")
+//                break
+//            }
+//            println("How many round-trip tickets: ")
+//            // numRoundTrip = Scanner(System.`in`).nextInt()
+//            numRoundTrip = readLine()!!.toInt()
+//            val trainTickets = TrainTickets(numTotal, numRoundTrip)
+//            if (trainTickets.checkOk()) {
+//                println("Total tickets: ${trainTickets.getTotalTickets()}")
+//                println("Round-trip: ${trainTickets.getRoundTripTotal()}")
+//                println("Total: ${trainTickets.getTotalPrice()}")
+//            }
+//        }
     } catch (e:Exception) {
-        println("Error input , Bye !!")
+        println("Error input , Exit !!")
     }
 }
 
-class TrainTickets(private val totalTickets:Int, private val roundTripTotal:Int) {
+class TrainTickets(private var totalTickets:Int, private var roundTripTotal:Int) {
       fun checkOk():Boolean {
           if(totalTickets<0 ) {
               println("Total tickets cannot be less than zero !!")
@@ -44,6 +61,12 @@ class TrainTickets(private val totalTickets:Int, private val roundTripTotal:Int)
               return false
           }
           return true
+      }
+      fun setTotalTickets(_totalTickets:Int) {
+          this.totalTickets = _totalTickets
+      }
+      fun setRoundTripTotal(_roundTripTotal:Int) {
+          this.roundTripTotal = _roundTripTotal
       }
       fun getTotalTickets()=this.totalTickets
       fun getRoundTripTotal()=this.roundTripTotal
